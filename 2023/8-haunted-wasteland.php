d1afb910312013502c24ec2bd519eeeb6890ea85
Tom Peters <tompeters@myonlinestore.com>
2023-12-08 12:01:43 +0100

    [2023] add day 8 solutions

---
+++ 2023/8-haunted-wasteland.php
@@ -0,0 +1,124 @@
+<?php
+
+$input = file('./input/8.txt');
+
+/**
+ * @param string[] $input
+ *
+ * @return array[]
+ */
+function processInput(array $input): array
+{
+    $directionString = trim(array_shift($input));
+    $directions = str_split($directionString);
+
+    $map = [];
+    $nodes = array_slice($input, 1);
+    foreach ($nodes as $node) {
+        // (BBB, BBB)
+        list($node, $leftRight) = explode('=', $node);
+        preg_match_all('/[0-9A-Z]+/', $leftRight, $matches);
+
+        $node = trim($node);
+        $map[$node] = [
+            'L' => trim($matches[0][0]),
+            'R' => trim($matches[0][1]),
+        ];
+    }
+
+    return [$directions, $map];
+}
+
+/**
+ * @param string[] $map
+ *
+ * @return string[]
+ */
+function findStartingNodes(array $map): array
+{
+    $startingNodes = [];
+    foreach ($map as $node => $potentialNodes) {
+        if (str_ends_with($node, 'A') === true) {
+            $startingNodes[] = $node;
+        }
+    }
+
+    return $startingNodes;
+}
+
+/**
+ * @param string[] $input
+ */
+function partOne(array $input): int
+{
+    list($directions, $map) = processInput($input);
+
+    $steps = 0;
+    $navigationPlan = [];
+    $node = 'AAA';
+    while ($node !== 'ZZZ') {
+        $potentialNodes = $map[$node];
+
+        if (empty($navigationPlan) === true) {
+            $navigationPlan = $directions;
+        }
+
+        $direction = array_shift($navigationPlan);
+        $node = $potentialNodes[$direction];
+        $steps++;
+    }
+
+    return $steps;
+}
+
+/**
+ * @param string[] $input
+ */
+function partTwo(array $input): int
+{
+    list($directions, $map) = processInput($input);
+
+    $steps = 0;
+    $navigationPlan = $stepsPerNode = [];
+    $nodes = findStartingNodes($map);
+    $simultaneousNodes = count($nodes);
+    do {
+        $steps++;
+        if (empty($navigationPlan) === true) {
+            $navigationPlan = $directions;
+        }
+
+        $direction = array_shift($navigationPlan);
+        foreach ($nodes as $index => $node) {
+            $potentialNodes = $map[$node];
+
+            $nextNode = $potentialNodes[$direction];
+
+            if (str_ends_with($nextNode, 'Z') === true) {
+                $stepsPerNode[] = $steps;
+                unset($nodes[$index]);
+            } else {
+                $nodes[$index] = $nextNode;
+            }
+        }
+    } while (count($stepsPerNode) < $simultaneousNodes);
+
+    $leastCommonMultiple = array_shift($stepsPerNode);
+    foreach ($stepsPerNode as $steps) {
+        $leastCommonMultiple = gmp_lcm($leastCommonMultiple, $steps);
+    }
+
+    return (int)$leastCommonMultiple;
+}
+
+$start = microtime(true);
+$solutionOne = partOne($input);
+$solutionTwo = partTwo($input);
+$end = microtime(true);
+
+echo '*-------------------------*' . PHP_EOL;
+echo 'Part 1: ' . $solutionOne . PHP_EOL;
+echo 'Part 2: ' . $solutionTwo . PHP_EOL;
+echo PHP_EOL;
+echo 'Completed in ' . number_format(($end - $start) * 1000, 2) . ' milliseconds!' . PHP_EOL;
+echo '*-------------------------*' . PHP_EOL;
