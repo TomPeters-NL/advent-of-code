d6662a01532ea9f3f07339e8aefaa8f49f0c90b0
Tom Peters <development@tompeters.dev>
2023-12-10 05:56:51 +0100

    [2015] add day 6 solutions

--- 2015/6-fire-hazard.php
+++ 2015/6-fire-hazard.php
@@ -21,32 +21,44 @@ function partOne(array $input): int
     $grid = createGrid();
 
     foreach ($input as $instruction) {
-        preg_match('/^(\D+)(\d+),(\d+) through (\d+),(\d+)$/', $instruction, $components);
-        $command = trim($components[1]);
-        $startX = (int) $components[2];
-        $startY = (int) $components[3];
-        $endX = (int) $components[4];
-        $endY = (int) $components[5];
+        preg_match('/^(\D+) (\d+),(\d+) through (\d+),(\d+)$/', $instruction, $components);
+        list($instruction, $command, $startX, $startY, $endX, $endY) = $components;
 
         $rangeX = range($startX, $endX);
         $rangeY = range($startY, $endY);
 
         switch ($command) {
             case 'turn on':
-                # code...
+                foreach ($rangeX as $x) {
+                    foreach ($rangeY as $y) {
+                        $grid[$x][$y] = 1;
+                    }
+                }
+
                 break;
             case 'turn off':
-                # code...
+                foreach ($rangeX as $x) {
+                    foreach ($rangeY as $y) {
+                        $grid[$x][$y] = 0;
+                    }
+                }
+
                 break;
             default:
                 foreach ($rangeX as $x) {
                     foreach ($rangeY as $y) {
-                        $grid[$x][$y] 
+                        $grid[$x][$y] = !$grid[$x][$y];
                     }
                 }
         }
+    }
 
-        return 1;
+    $lit = 0;
+    foreach ($grid as $x) {
+        $lit += array_sum($x);
+    }
+
+    return $lit;
 }
 
 /**
@@ -54,11 +66,63 @@ function partOne(array $input): int
  */
 function partTwo(array $input): int
 {
-    return 2;
+    $grid = createGrid();
+
+    foreach ($input as $instruction) {
+        preg_match('/^(\D+) (\d+),(\d+) through (\d+),(\d+)$/', $instruction, $components);
+        list($instruction, $command, $startX, $startY, $endX, $endY) = $components;
+
+        $rangeX = range($startX, $endX);
+        $rangeY = range($startY, $endY);
+
+        switch ($command) {
+            case 'turn on':
+                foreach ($rangeX as $x) {
+                    foreach ($rangeY as $y) {
+                        $grid[$x][$y]++;
+                    }
+                }
+
+                break;
+            case 'turn off':
+                foreach ($rangeX as $x) {
+                    foreach ($rangeY as $y) {
+                        if ($grid[$x][$y] > 0) {
+                            $grid[$x][$y]--;
+                        }
+                    }
+                }
+
+                break;
+            default:
+                foreach ($rangeX as $x) {
+                    foreach ($rangeY as $y) {
+                        $grid[$x][$y] = $grid[$x][$y] + 2;
+                    }
+                }
+        }
+    }
+
+    $totalBrightness = 0;
+    foreach ($grid as $x) {
+        $totalBrightness += array_sum($x);
+    }
+
+    return $totalBrightness;
 }
 
+###############
+### Results ###
+###############
+
+$start = microtime(true);
 $solutionOne = partOne($input);
 $solutionTwo = partTwo($input);
+$end = microtime(true);
 
+echo '*-------------------------*' . PHP_EOL;
 echo 'Part 1: ' . $solutionOne . PHP_EOL;
 echo 'Part 2: ' . $solutionTwo . PHP_EOL;
+echo PHP_EOL;
+echo 'Completed in ' . number_format(($end - $start) * 1000, 2) . ' milliseconds!' . PHP_EOL;
+echo '*-------------------------*' . PHP_EOL;
