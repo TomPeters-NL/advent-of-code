35f5b0ea8d3ce3e728a4c302f25b8db1565e05a0
Tom Peters <development@tompeters.dev>
2023-12-13 23:59:45 +0100

    [2023] add efficient day 12 solution (thanks reddit)

--- 2023/12-hot-springs.php
+++ 2023/12-hot-springs.php
@@ -1,135 +1,85 @@
 <?php
 
-$input = file('./input/12-test.txt');
+$input = file('./input/12.txt');
 
 /**
  * @param string $record
  * @param int[] $groupSizes
- * @return array<int[], string>
+ * @return array
  */
-function unfold(string $record, array $groupSizes): array
+function unfold(string $record, array $groups): array
 {
-    $record .= '?';
-    $unfoldedRecord = str_repeat($record, 5);
-    $unfoldedRecord = substr($unfoldedRecord, 0, strlen($unfoldedRecord) - 1);
+    $records = array_fill(0, 5, $record);
+    $unfoldedRecord = implode('?', $records);
 
-    $groupSizeString = implode('', $groupSizes);
-    $unfoldedGroupSizes = str_repeat($groupSizeString, 5);
-    $unfoldedGroupSizes = array_map('intval', str_split($unfoldedGroupSizes));
+    $groups = array_fill(0, 5, $groups);
+    $unfoldedGroups = array_merge(...$groups);
 
-    return [$unfoldedRecord, $unfoldedGroupSizes];
+    return [$unfoldedRecord, $unfoldedGroups];
 }
 
-/**
- * @param int[] $groupSizes
- */
-function validateCompleteRecord(string $record, array $groupSizes): bool
+function calculateArrangements(string $record, array $groups, array &$cache = []): int
 {
-    preg_match_all('/#+/', $record, $matches);
-    $groups = $matches[0];
+    // Check the cache for reoccuring combinations.
+    $cacheRecord = $record . '-' . implode(',', $groups);
+    if (array_key_exists($cacheRecord, $cache) === true) {
+        return $cache[$cacheRecord];
+    }
 
-    $isValidGroupAmount = count($groups) === count($groupSizes);
+    // Once the last of the record has been checked, check if all groups were handled.
+    if ($record === '') {
+        return (int) empty($groups);
+    }
 
-    if ($isValidGroupAmount === true) {
-        $hasValidGroupSizes = false;
+    // Once all groups have been handled, check whether there are no more springs left in the record.
+    // Returns "1" if all springs were handled, "0" if there are any springs left.
+    if (empty($groups) === true) {
+        return (int) !str_contains($record, '#');
+    }
 
-        foreach ($groups as $index => $group) {
-            $hasValidGroupSizes = strlen($group) === $groupSizes[$index];
+    // Initialize the arrangement sum.
+    $arrangements = 0;
 
-            if ($hasValidGroupSizes === false) {
-                break;
+    // Check if the string starts with an empty or damaged space (potential empty space).
+    // If so, yeet it into the next iteration without that first character.
+    $startsWithoutSpring = str_starts_with($record, '.') || str_starts_with($record, '?');
+    if ($startsWithoutSpring === true) {
+        $nextRecord = substr($record, 1);
+
+        $arrangements += calculateArrangements($nextRecord, $groups, $cache);
+    }
+
+    // Check if the string starts with a spring or damaged space (potential spring).
+    $startsWithSpring = str_starts_with($record, '#') || str_starts_with($record, '?');
+
+    if ($startsWithSpring === true) {
+        // Check whether there is enough left of the record for the next group length.
+        $groupLength = $groups[0];
+        $enoughRecordLeft = strlen($record) >= $groupLength;
+
+        if ($enoughRecordLeft === true) {
+            // Check whether the group would contain any empty spaces, if grouped as-is.
+            $group = substr($record, 0, $groupLength);
+            $emptySpacesInGroup = str_contains($group, '.');
+
+            // Check whether the group would be followed by a spring if grouped as-is.
+            $isDiscreteGroup = ($record[$groupLength] ?? '') !== '#';
+
+            if ($emptySpacesInGroup === false && $isDiscreteGroup === true) {
+                // With a succesful grouping completed, generate the remaining record and groups.
+                // The "substr()" offset is increased by "1" to include the empty space required after the group.
+                $remainingRecord = substr($record, $groupLength + 1);
+                $remainingGroups = array_slice($groups, 1);
+
+                $arrangements += calculateArrangements($remainingRecord, $remainingGroups, $cache);
             }
         }
     }
 
-    return $isValidGroupAmount && $hasValidGroupSizes;
-}
+    // Add the current record to the cache.
+    $cache[$cacheRecord] = $arrangements;
 
-/**
- * @param int[] $groupSizes
- */
-function validatePartialRecord(string $record, array $groupSizes): bool
-{
-    // Extract the groups currently present in the partial record.
-    preg_match_all('/#+/', $record, $matches);
-    $currentGroups = $matches[0];
-    $groupCount = count($currentGroups);
-
-    // Check whether the current record does not exceed the number of required groups.
-    $validGroupCount = $groupCount <= count($groupSizes);
-
-    // Check whether the existing groups adhere to the required length.
-    $validGroupLength = $groupCount === 0;
-    if ($validGroupCount === true && $groupCount > 0) {
-        foreach ($currentGroups as $index => $group) {
-            $validGroupLength = strlen($group) <= $groupSizes[$index];
-
-            if ($validGroupLength === false) {
-                break;
-            }
-        }
-    }
-
-    return $validGroupCount && $validGroupLength;
-}
-
-/**
- * @param string[] $arrangements
- * @param int[] $groupSizes
- * @param string[] $cache
- */
-function iterateRecord(array &$arrangements, string $record, array $groupSizes, array &$cache): void
-{
-    $isInCache = array_key_exists($record, $cache);
-    $isDamagedRecord = str_contains($record, '?');
-
-    if ($isInCache === false && $isDamagedRecord === true) {
-        // Cache the current arrangement.
-        $cache[$record] = $record;
-
-        // Validate current progress.
-        $target = strpos($record, '?');
-        $progress = substr($record, 0, $target + 1);
-        $isValidRecord = validatePartialRecord($progress, $groupSizes);
-
-        // Create new arrangements.
-        $iteration1 = $iteration2 = $record;
-
-        $iteration1[$target] = '.';
-        $iteration2[$target] = '#';
-
-        // Continue iteration process.
-        if ($isValidRecord === true) {
-            iterateRecord($arrangements, $iteration1, $groupSizes, $cache);
-            iterateRecord($arrangements, $iteration2, $groupSizes, $cache);
-        }
-    }
-
-    if ($isInCache === false && $isDamagedRecord === false) {
-        // Cache the current arrangement.
-        $cache[$record] = $record;
-
-        // Check if the arrangement matches conditions.
-        $isValidRecord = validateCompleteRecord($record, $groupSizes);
-
-        // Increase the sum if valid.
-        if ($isValidRecord === true) {
-            $arrangements[] = $record;
-        }
-    }
-}
-
-/**
- * @param int[] $groupSizes
- */
-function calculateArrangementSum(string $record, array $groupSizes): int
-{
-    $arrangements = [];
-    $cache = [];
-
-    iterateRecord($arrangements, $record, $groupSizes, $cache);
-
-    return count($arrangements);
+    return $arrangements;
 }
 
 /**
@@ -140,11 +90,11 @@ function partOne(array $input): int
     $arrangementSum = 0;
 
     foreach ($input as $springRecord) {
-        list($record, $groupSizes) = explode(' ', $springRecord);
+        list($record, $groups) = explode(' ', $springRecord);
         $record = trim($record);
-        $groupSizes = array_map('intval', explode(',', trim($groupSizes)));
+        $groups = array_map('intval', explode(',', trim($groups)));
 
-        $arrangementSum += calculateArrangementSum($record, $groupSizes);
+        $arrangementSum += calculateArrangements($record, $groups);
     }
 
     return $arrangementSum;
@@ -158,12 +108,13 @@ function partTwo(array $input): int
     $arrangementSum = 0;
 
     foreach ($input as $springRecord) {
-        list($record, $groupSizes) = explode(' ', $springRecord);
+        list($record, $groups) = explode(' ', $springRecord);
         $record = trim($record);
-        $groupSizes = array_map('intval', explode(',', trim($groupSizes)));
+        $groups = array_map('intval', explode(',', trim($groups)));
 
-        list($record, $groupSizes) = unfold($record, $groupSizes);
-        $arrangementSum += calculateArrangementSum($record, $groupSizes);
+        list($unfoldedRecord, $unfoldedGroups) = unfold($record, $groups);
+
+        $arrangementSum += calculateArrangements($unfoldedRecord, $unfoldedGroups);
     }
 
     return $arrangementSum;
