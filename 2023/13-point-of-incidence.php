dec1ae0488eea0835f6aad6d46b3c1d422913f1f
Tom Peters <development@tompeters.dev>
2023-12-13 21:26:59 +0100

    [2023] add day 13-2 solution

--- 2023/13-point-of-incidence.php
+++ 2023/13-point-of-incidence.php
@@ -25,31 +25,72 @@ function extractPatterns(array $input): array
     return $patterns;
 }
 
+function findDifferences(string $alpha, string $beta): int
+{
+    $differences = 0;
+
+    // If the strings are equal, skip the differentiation process.
+    $equality = $alpha === $beta;
+
+    if ($equality === false) {
+        $length = strlen($alpha);
+
+        // Determine and count differences between strings.
+        for ($index = 0; $index < $length; $index++) {
+            if ($alpha[$index] !== $beta[$index]) {
+                $differences++;
+            }
+        }
+    }
+
+    // var_dump("Comparing '$alpha' and '$beta'. Found $differences differences.");
+
+    return $differences;
+}
+
 /**
  * @param string[] $pattern
  */
-function isPerfectReflection(array $pattern, int $mirrorIndex): bool
+function isPerfectReflection(array $pattern, int $mirrorIndex, bool $smudgedMirrors = false): bool
 {
     $isPerfectReflection = false;
 
     $patternStart = array_key_first($pattern);
     $patternEnd = count($pattern);
     $counterIndex = $mirrorIndex - 1;
+    $smudges = 0;
 
     for ($index = $mirrorIndex; $index < $patternEnd; $index++) {
+        // Check if there is still room at the start of the pattern.
         $isValidCounterIndex = $patternStart <= $counterIndex;
         if ($isValidCounterIndex === false) {
             break;
         }
 
-        $isMirrorImage = $pattern[$index] === $pattern[$counterIndex];
-        if ($isMirrorImage === true) {
+        // Extract the patterns.
+        $currentPattern = $pattern[$index];
+        $counterPattern = $pattern[$counterIndex];
+
+        // Compare the patterns.
+        $patternDifferences = findDifferences($currentPattern, $counterPattern);
+        $smudges += $patternDifferences;
+
+        // Depending on whether mirrors are expected to have a single smudge or not, the tolerance for differences is either 0 or 1.
+        if (($smudgedMirrors === false && $smudges === 0) || ($smudgedMirrors === true && $smudges <= 1)) {
             $isPerfectReflection = true;
-            $counterIndex--;
-        } else {
-            $isPerfectReflection = false;
+        }
+
+        if (($smudgedMirrors === false && $smudges > 0) || ($smudgedMirrors === true && $smudges > 1)) {
             break;
         }
+
+        // Reduce the counter index for the next loop.
+        $counterIndex--;
+    }
+
+    // Perform a final reflection check, expected either exactly 0 or 1 smudges, depending on the settings.
+    if (($smudgedMirrors === false && $smudges !== 0) || ($smudgedMirrors === true && $smudges !== 1)) {
+        $isPerfectReflection = false;
     }
 
     return $isPerfectReflection;
@@ -58,32 +99,27 @@ function isPerfectReflection(array $pattern, int $mirrorIndex): bool
 /**
  * @param string[] $pattern
  */
-function findHorizontalReflection(array $pattern): bool|int
+function findHorizontalReflection(array $pattern, bool $smudgedMirrors = false): bool|int
 {
     $mirrorIndex = false;
 
     // Find the mirror point.
-    $previousLine = '';
+    $previousLine = str_repeat('x', strlen($pattern[0]));
     foreach ($pattern as $index => $line) {
-        if ($line === $previousLine) {
+        if ($line === $previousLine || findDifferences($line, $previousLine) === 1) {
             $mirrorIndex = $index;
-            $isPerfectReflection = isPerfectReflection($pattern, $mirrorIndex);
+
+            // Check if it is a perfect reflection.
+            $isPerfectReflection = isPerfectReflection($pattern, $mirrorIndex, $smudgedMirrors);
             if ($isPerfectReflection === true) {
                 break;
+            } else {
+                // If not, reset the mirror index.
+                $mirrorIndex = false;
             }
         }
 
         $previousLine = $line;
-    }
-
-    // Check if it is a perfect reflection.
-    if ($mirrorIndex !== false) {
-        $isPerfectReflection = isPerfectReflection($pattern, $mirrorIndex);
-
-        // If not, do not provide mirror index.
-        if ($isPerfectReflection === false) {
-            $mirrorIndex = false;
-        }
     }
 
     return $mirrorIndex;
@@ -92,7 +128,7 @@ function findHorizontalReflection(array $pattern): bool|int
 /**
  * @param string[] $pattern
  */
-function findVerticalReflection(array $pattern): bool|int
+function findVerticalReflection(array $pattern, bool $smudgedMirrors = false): bool|int
 {
     // Enable the pattern to be transposed clockwise.
     $reversedPattern = array_reverse($pattern);
@@ -107,7 +143,7 @@ function findVerticalReflection(array $pattern): bool|int
     }
 
     // Find the mirror index, if any.
-    return findHorizontalReflection($transposedPattern);
+    return findHorizontalReflection($transposedPattern, $smudgedMirrors);
 }
 
 /**
@@ -118,9 +154,11 @@ function partOne(array $input): int
     $patterns = extractPatterns($input);
 
     $mirrors = [];
-    foreach ($patterns as $index => $pattern) {
-        findVerticalReflection($pattern);
+    foreach ($patterns as $pattern) {
+        // Find a horizontal reflection pattern.
         $horizontal = findHorizontalReflection($pattern);
+
+        // If no horizontal pattern was found, find the vertical reflection pattern.
         $vertical = $horizontal === false ? findVerticalReflection($pattern) : false;
 
         if ($horizontal !== false) {
@@ -138,11 +176,24 @@ function partOne(array $input): int
  */
 function partTwo(array $input): int
 {
-    // For future Tom.
-    // Part 2 might be almost identical to part 1.
-    // Try to implement a mandatory error margin of 1 in the perfect reflection validation.
+    $patterns = extractPatterns($input);
 
-    return 2;
+    $mirrors = ['horizontal' => [], 'vertical' => []];
+    foreach ($patterns as $pattern) {
+        // Find a horizontal reflection pattern.
+        $horizontal = findHorizontalReflection($pattern, true);
+
+        // If no horizontal pattern was found, find the vertical reflection pattern.
+        $vertical = $horizontal === false ? findVerticalReflection($pattern, true) : false;
+
+        if ($horizontal !== false) {
+            $mirrors['horizontal'][] = $horizontal;
+        } elseif ($vertical !== false) {
+            $mirrors['vertical'][] = $vertical;
+        }
+    }
+
+    return array_sum($mirrors['vertical']) + (100 * array_sum($mirrors['horizontal']));
 }
 
 ###############
