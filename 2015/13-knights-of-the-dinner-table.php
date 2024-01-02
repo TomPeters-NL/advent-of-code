1bbe6caa75f7cdf855d9899924a8a554f6987f8e
Tom Peters <tompeters@myonlinestore.com>
2024-01-02 18:59:40 +0100

    [2015] add solutions for day 13 and 14

---
+++ 2015/13-knights-of-the-dinner-table.php
@@ -0,0 +1,176 @@
+<?php
+
+######################
+### Initialization ###
+######################
+
+require_once(__DIR__ . '/../helper/AdventHelper.php');
+
+use AdventOfCode\Helper\AdventHelper;
+
+$adventHelper = new AdventHelper();
+
+$input = file('./input/13', FILE_IGNORE_NEW_LINES);
+
+#################
+### Solutions ###
+#################
+
+/**
+ * Parses each input line for the relevant data to map happiness scores between guests.
+ *
+ * @param string[] $input The puzzle input.
+ *
+ * @return int[][] A list of interpersonal happiness scores.
+ */
+function extractHappinessMapping(array $input, bool $includeMe = false): array
+{
+    $mapping = [];
+
+    foreach ($input as $line) {
+        preg_match('/([A-Z][a-z]+).+(gain|lose)\D+(\d+).+([A-Z][a-z]+)/', $line, $matches);
+
+        [$ignore, $subject, $type, $happiness, $person] = $matches;
+
+        $happinessScore = match ($type) {
+            'gain' => $happiness,
+            'lose' => '-' . $happiness,
+        };
+
+        $mapping[$subject][$person] = (int) $happinessScore;
+    }
+
+    if ($includeMe === true) {
+        foreach ($mapping as $subject => $subjectMapping) {
+            $mapping[$subject]['Me'] = 0;
+            $mapping['Me'][$subject] = 0;
+        }
+    }
+
+    return $mapping;
+}
+
+/**
+ * Generates all potential combinations of seating arrangements.
+ *
+ * @param string[][] $arrangements The current list of possible seating arrangements.
+ * @param string[]   $guests       The list of all potential guests.
+ *
+ * @return string[][] The new, expanded list of possible seating arrangements.
+ */
+function generateArrangements(array $arrangements, array $guests): array
+{
+    $newArrangements = 0;
+
+    foreach ($arrangements as $index => $arrangement) {
+        foreach ($guests as $guest) {
+            # Skip if the guest is already in the arrangement.
+            if (in_array($guest, $arrangement) === true) {
+                continue;
+            }
+
+            # Add the current guest to create a new arrangement.
+            $newArrangement = $arrangement;
+            $newArrangement[] = $guest;
+
+            # Add the new arrangement to the list and increment the counter.
+            $arrangements[] = $newArrangement;
+            $newArrangements++;
+        }
+
+        # Only remove the current arrangement if it produced new arrangements.
+        if ($newArrangements > 0) {
+            unset($arrangements[$index]);
+        }
+    }
+
+    # If new arrangements are still being formed, continue the recursive loop.
+    if ($newArrangements > 0) {
+        $arrangements = generateArrangements($arrangements, $guests);
+    }
+
+    return $arrangements;
+}
+
+/**
+ * Creates a list of possible seating arrangements.
+ *
+ * @param array $guests The list of guests.
+ *
+ * @return string[][] A list of all potential seating arrangement combinations.
+ */
+function createSeatingArrangements(array $guests): array
+{
+    # Initialize the seating arrangements.
+    $seating = [];
+    foreach ($guests as $guest) {
+        $seating[][] = $guest;
+    }
+
+    return generateArrangements($seating, $guests);
+}
+
+function calculateHappinessScores(array $mapping, array $seating): array
+{
+    $scores = [];
+
+    foreach ($seating as $arrangement) {
+        $score = 0;
+
+        $first = array_key_first($arrangement);
+        $last = array_key_last($arrangement);
+
+        foreach ($arrangement as $guestIndex => $guest) {
+            $neighbourRight = $guestIndex === $first ? $arrangement[$last] : $arrangement[$guestIndex - 1];
+            $neighbourLeft = $guestIndex === $last ? $arrangement[$first] : $arrangement[$guestIndex + 1];
+
+            $score += $mapping[$guest][$neighbourRight];
+            $score += $mapping[$guest][$neighbourLeft];
+        }
+
+        $implodedArrangement = implode('-', $arrangement);
+        $scores[$implodedArrangement] = $score;
+    }
+
+    return $scores;
+}
+
+/**
+ * Returns the solution for the first part of this day's puzzle.
+ *
+ * @param string[] $input The puzzle input.
+ */
+function partOne(array $input): int
+{
+    $happinessMapping = extractHappinessMapping($input);
+
+    $guests = array_keys($happinessMapping);
+    $seatingArrangements = createSeatingArrangements($guests);
+
+    $happinessScores = calculateHappinessScores($happinessMapping, $seatingArrangements);
+
+    return max($happinessScores);
+}
+
+/**
+ * Returns the solution for the second part of this day's puzzle.
+ *
+ * @param string[] $input The puzzle input.
+ */
+function partTwo(array $input): int
+{
+    $happinessMapping = extractHappinessMapping($input, true);
+
+    $guests = array_keys($happinessMapping);
+    $seatingArrangements = createSeatingArrangements($guests);
+
+    $happinessScores = calculateHappinessScores($happinessMapping, $seatingArrangements);
+
+    return max($happinessScores);
+}
+
+###############
+### Results ###
+###############
+
+$adventHelper->printSolutions(partOne($input), partTwo($input));
---
+++ 2015/14-reindeer-olympics.php
@@ -0,0 +1,169 @@
+<?php
+
+######################
+### Initialization ###
+######################
+
+require_once(__DIR__ . '/../helper/AdventHelper.php');
+
+use AdventOfCode\Helper\AdventHelper;
+
+$adventHelper = new AdventHelper();
+
+$input = file('./input/14', FILE_IGNORE_NEW_LINES);
+
+#################
+### Solutions ###
+#################
+
+/**
+ * Analyze each reindeer's description for their stats.
+ *
+ * @param string[] $input The puzzle input.
+ *
+ * @return array A list of each reindeer's stats.
+ */
+function analyzeReindeer(array $input): array
+{
+    $reindeer = [];
+
+    foreach ($input as $line) {
+        preg_match('/([A-Z][a-z]+)\D+(\d+)\D+(\d+)\D+(\d+)/', $line, $matches);
+
+        [$ignore, $name, $speed, $stamina, $rest] = $matches;
+
+        $reindeer[$name] = [$name, (int) $speed, (int) $stamina, (int) $rest];
+    }
+
+    return $reindeer;
+}
+
+/**
+ * Calculate the distance traveled for a single reindeer at a given point in time.
+ *
+ * @param array $reindeer A single reindeer's stats.
+ * @param int   $seconds  The time in seconds for which the distance traveled should be calculated.
+ *
+ * @return int The distance traveled by the reindeer.
+ */
+function calculateDistanceAtTime(array $reindeer, int $seconds): int
+{
+    [$name, $speed, $stamina, $rest] = $reindeer;
+
+    # Determine the length of a full cycle, and the distance a reindeer can travel in it.
+    $cycle = $stamina + $rest;
+    $cycleDistance = $speed * $stamina;
+
+    # Determine the amount of full cycles and the distance traveled in them.
+    $fullCycles = (int) floor($seconds / $cycle);
+    $fullDistance = $fullCycles * $cycleDistance;
+
+    # Determine the partial cycle length and the distance traveled in it.
+    $partialCycle = ($seconds - ($fullCycles * $cycle));
+    $partialDistance = $partialCycle > $stamina ? $cycleDistance : $partialCycle * $speed;
+
+    return $fullDistance + $partialDistance;
+}
+
+/**
+ * Calculate the distance traveled by each reindeer according to Santa's classic rules.
+ *
+ * @param array $reindeer A list of each reindeer's stats.
+ * @param int   $seconds  The race duration in seconds.
+ *
+ * @return int[]
+ */
+function calculateDistances(array $reindeer, int $seconds): array
+{
+    $distances = [];
+
+    foreach ($reindeer as $currentReindeer) {
+        $name = $currentReindeer[0];
+
+        $distances[$name] = calculateDistanceAtTime($currentReindeer, $seconds);
+    }
+
+    return $distances;
+}
+
+/**
+ * Calculates the points each reindeer earns during the race under Santa's new rules.
+ *
+ * @param array $reindeer A list of each reindeer's stats.
+ * @param int   $seconds The length of the race in seconds.
+ *
+ * @return int[] The points each reindeer has earned.
+ */
+function calculatePoints(array $reindeer, int $seconds): array
+{
+    $distances = [];
+    $time = range(1, $seconds);
+
+    # For each second, determine the reindeer in the lead and log them.
+    foreach ($time as $second) {
+        $distanceAtTime = [];
+
+        foreach ($reindeer as $focusReindeer) {
+            $distanceAtTime[$focusReindeer[0]] = calculateDistanceAtTime($focusReindeer, $second);
+        }
+
+        arsort($distanceAtTime);
+        $leadValue = $distanceAtTime[array_key_first($distanceAtTime)];
+
+        # As ties are possible, check for multiple occurrences of the same leading distance.
+        foreach ($distanceAtTime as $name => $distance) {
+            if ($distance === $leadValue) {
+                $distances[$second][] = $name;
+            } else {
+                break;
+            }
+        }
+    }
+
+    $reindeerNames = array_keys($reindeer);
+    $reindeerStarts = array_fill(0, count($reindeer), 0);
+    $points = array_combine($reindeerNames, $reindeerStarts);
+
+    # Calculate the points for each time a reindeer is in the lead.
+    foreach ($distances as $names) {
+        foreach ($names as $name) {
+            $points[$name]++;
+        }
+    }
+
+    return $points;
+}
+
+/**
+ * Returns the solution for the first part of this day's puzzle.
+ *
+ * @param string[] $input The puzzle input.
+ */
+function partOne(array $input): int
+{
+    $reindeer = analyzeReindeer($input);
+
+    $distances = calculateDistances($reindeer, 2503);
+
+    return max($distances);
+}
+
+/**
+ * Returns the solution for the second part of this day's puzzle.
+ *
+ * @param string[] $input The puzzle input.
+ */
+function partTwo(array $input): int
+{
+    $reindeer = analyzeReindeer($input);
+
+    $points = calculatePoints($reindeer, 2503);
+
+    return max($points);
+}
+
+###############
+### Results ###
+###############
+
+$adventHelper->printSolutions(partOne($input), partTwo($input));
